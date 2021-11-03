bd4ed63ca00e8688a1748afb6e396a19acdadb09
pierre richaud <pierrerichaud@MacBook-Pro-de-pierre.local>
2021-11-03 14:45:46 +0100

    Ajout PersonaOgre, CarConfiguration et calcul de la conso associée à la voiture.

---
+++ server/app/OGREConstants.js
@@ -0,0 +1,10 @@
+const OGREConstants = {
+    CONSUMPTION100KM: 7,
+    CALORIFIC_VALUE: 10,
+    MOTOR_TYPE: {
+        DIESEL: 'Diesel',
+        ELECTRIQUE: 'Electrique',
+        HYBRIDE: 'Hybride'
+    }
+}
+module.exports = OGREConstants
