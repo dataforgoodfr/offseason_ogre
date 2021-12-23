664ede5150ae1cf3cbb3e0fec2578bd117931e27
Rémi <remi.boivin@epitech.eu>
2021-12-23 11:48:04 -0500

    create config folder and move database.js into it
    Use a config folder permit a better understanding of database.js

--- server/app/dataMappers/playerMapper.js
+++ server/app/dataMappers/playerMapper.js
@@ -1,5 +1,5 @@
 const PersonaOgre = require('../models/PersonaOgre');
-const db = require('../database');
+const db = require('../config/database');
 
 const playerMapper = {};
 
--- server/app/models/PersonaOgre.js
+++ server/app/models/PersonaOgre.js
@@ -1,4 +1,4 @@
-const db = require('../database');
+const db = require('../config/database');
 
 const PersonaOgre = class {
     constructor(obj) {
