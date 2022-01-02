c21a1dfa86e15dcc6d0262e6b1ed8bde32aa4500
Vladimir <vladimir@DESKTOP-GC7CN5C.localdomain>
2022-01-02 16:04:08 +0100

    added sequelize model Player

--- server/test_sequelize.js
+++ server/test_sequelize.js
@@ -1,31 +1,33 @@
 require('dotenv').config({ path: __dirname + '/.env'});
 const sequelize = require('./app/config/databasebis');
-
-testseq = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
-
-testseq();
-
 const CarConsumption = require('./app/models/CarConsumption');
-const car1 = CarConsumption.build({
+const Player = require('./app/models/Player');
+
+playerData = {
+    adultsPerHousehold:2,
+    childrenPerHousehold:3
+}
+carData = {
     hasCar:true,
     carShareDistance: 1000,
     aloneDistance: 5000,
     withHouseholdDistance: 2000,
     litresPer100km:10
-});
-// console.log(car1);
-console.log('personsPerHousehold',car1.personsPerHousehold);
-console.log('dailyAloneDistance',car1.dailyAloneDistance);
-console.log('dailyWithHouseholdDistance',car1.dailyWithHouseholdDistance);
-console.log('distancePerLiter',car1.distancePerLiter);
-console.log('aloneConsumption',car1.aloneConsumption);
-console.log('withHouseholdConsumption',car1.withHouseholdConsumption);
-console.log('carShareConsumption',car1.carShareConsumption);
-console.log('carConsumption',car1.carConsumption);
+}
+
+test_sequelize = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync({ force: true });
+        const car = await CarConsumption.create(carData);
+        const player = await Player.create(playerData);
+        
+        
+        console.log(car.toJSON());
+        console.log(player.toJSON());    
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+test_sequelize();
