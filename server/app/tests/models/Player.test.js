48e941517c1a1d3b809269d2d65b45d0087267ce
Rémi Boivin <remi.boivin@epitech.eu>
2022-01-07 00:44:12 +0100

    feat: Add Player.test.js
    Add an exemple of test by testing player Model.

--- server/app/tests/models/Player.test.js
+++ server/app/tests/models/Player.test.js
@@ -1,41 +1,9 @@
-const {mock} = require("sinon");
 const assert = require("assert").strict;
-const {playerModel} = require("../../models/Player");
-const {Player} = require("../../models");
+const Player = require('../../models/Player')
 
-let mockedFn = jest.FnInstance;
-
-jest.mock("../../models/Player");
-
-const mocked_player = jest.MockedFunction(Player);
-
-describe("test consumption calculation", () => {
-    beforeEach(() => {
-
-        jest.resetModules();
-//        mockedFn = jest.fn();
-    });
-
-  //  afterEach(() => {
-        // Reset to the original method implementation (non-mocked) and clear all the mock data
-    //    mockedFn.mockRestore();
-    //});
-    const getNow = () => new Date(Date.now());
-    it('does third thing', () => {
-        //player = mockedFn.mockImplementationOnce({id: 1})
-        player = jest
-            .spyOn(playerModel, 'findByPk')
-            .mockReturnValueOnce(() =>
-                Promise.resolve({id: 1, name: "test", consumption: 0, lastConsumption: getNow()})
-            );
-
-        test = jest.doMock('../../models/Player', () => ({
-            findByPk: jest.fn(() => {
-                return Promise.resolve({id: 1, name: "test", consumption: 0, lastConsumption: getNow()})
-            })
-        }));
-
-        console.log(test);
-        /* Another test with the default mock implementation */
+describe("test nb of persons per house for a Player ", () => {
+    it('Should compute in case: 2 adults and 3 childrens', async () => {
+        const player = await Player.findByPk(1)
+        assert.equal(player.personsPerHousehold, player.adultsPerHousehold + player.childrenPerHousehold)
     });
 });
