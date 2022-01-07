938fe26a9bd740275290a15f70cca3ae318897e9
Rémi Boivin <remi.boivin@epitech.eu>
2022-01-07 23:37:01 +0100

    refactor: change config database's path

--- server/app/controllers/playerController.js
+++ server/app/controllers/playerController.js
@@ -1,15 +1,15 @@
-const { Player, PlaneConsumption } = require('../models');
-const { CarConsumption } = require('../models');
+const { Player, PlaneConsumption } = require('../db/models');
+const { CarConsumption } = require('../db/models');
 const playerController = {}
 
-playerController.allPlayers = async (_, response, next) => {
+playerController.allPlayers = async(_, response, next) => {
     console.log('enter playerController.allPlayers');
     try {
         const players = await Player.findAll({
             include: [CarConsumption, PlaneConsumption]
         });
         response.json(players);
-    } catch(error) {
+    } catch (error) {
         throw new Error(error);
     }
 }
