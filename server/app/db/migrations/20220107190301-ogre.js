699c6bc78f0d76ce3a739abe0ab7da60a02b0e32
Rémi Boivin <remi.boivin@epitech.eu>
2022-01-07 23:50:08 +0100

    fix: Rename update_at into updated_at

--- server/app/db/migrations/20220107190301-ogre.js
+++ server/app/db/migrations/20220107190301-ogre.js
@@ -3,9 +3,9 @@
 /* istanbul ignore file */
 module.exports = {
     up: async(queryInterface, Sequelize) => {
-        await queryInterface.createTable('car_consumption', { id: Sequelize.INTEGER, has_car: Sequelize.BOOLEAN, car_share_distance: Sequelize.INTEGER, alone_distance: Sequelize.INTEGER, with_household_distance: Sequelize.INTEGER, litres_per100km: Sequelize.INTEGER, motor_type: Sequelize.INTEGER, created_at: Sequelize.DATE, update_at: Sequelize.DATE });
-        await queryInterface.createTable('plane_consumption', { id: Sequelize.INTEGER, distance_per_year: Sequelize.INTEGER, created_at: Sequelize.DATE, update_at: Sequelize.DATE });
-        await queryInterface.createTable('player', { id: Sequelize.INTEGER, adults_per_household: Sequelize.INTEGER, children_per_household: Sequelize.INTEGER, car_consumption_id: Sequelize.INTEGER, plane_consumption_id: Sequelize.INTEGER, created_at: Sequelize.DATE, update_at: Sequelize.DATE });
+        await queryInterface.createTable('car_consumption', { id: Sequelize.INTEGER, has_car: Sequelize.BOOLEAN, car_share_distance: Sequelize.INTEGER, alone_distance: Sequelize.INTEGER, with_household_distance: Sequelize.INTEGER, litres_per100km: Sequelize.INTEGER, motor_type: Sequelize.INTEGER, created_at: Sequelize.DATE, updated_at: Sequelize.DATE });
+        await queryInterface.createTable('plane_consumption', { id: Sequelize.INTEGER, distance_per_year: Sequelize.INTEGER, created_at: Sequelize.DATE, updated_at: Sequelize.DATE });
+        await queryInterface.createTable('player', { id: Sequelize.INTEGER, adults_per_household: Sequelize.INTEGER, children_per_household: Sequelize.INTEGER, car_consumption_id: Sequelize.INTEGER, plane_consumption_id: Sequelize.INTEGER, created_at: Sequelize.DATE, updated_at: Sequelize.DATE });
     },
 
     down: async(queryInterface, Sequelize) => {
