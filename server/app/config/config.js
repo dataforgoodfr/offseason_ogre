69cfb4db1d33f6b33f9ade8d6db18de546404012
Rémi Boivin <remi.boivin@epitech.eu>
2022-01-07 23:06:05 +0100

    feat: Add sequelize-cli config
    Add config to customize defaults folders

---
+++ server/app/config/config.js
@@ -0,0 +1,20 @@
+/* istanbul ignore file */
+
+var path = require('path')
+const dotenv = require("dotenv");
+dotenv.config(path.resolve('./', '.env'));
+
+module.exports = {
+    "development": {
+        "url": process.env.PG_URL,
+        "dialect": "postgres"
+    },
+    "test": {
+        "url": process.env.PG_URL_TEST,
+        "dialect": "postgres"
+    },
+    "production": {
+        "url": process.env.PG_URL_PRODUCTION,
+        "dialect": "postgres"
+    }
+}
