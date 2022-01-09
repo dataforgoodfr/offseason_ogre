3d2fece57696df15b64a9fada87713a35420d923
Rémi Boivin <remi.boivin@epitech.eu>
2022-01-09 02:54:16 +0100

    feat: Add player crud functions

--- server/app/controllers/playerController.js
+++ server/app/controllers/playerController.js
@@ -8,4 +8,24 @@ playerController.index = async(_, response, next) => {
     ressourceController.index(_, response, next, Player, [PlaneConsumption, CarConsumption]);
 }
 
+playerController.show = async(_, response, next) => {
+    console.log('enter playerController.show');
+    ressourceController.show(_, response, next, Player, [PlaneConsumption, CarConsumption]);
+}
+
+playerController.create = async(_, response, next) => {
+    console.log('enter playerController.create');
+    ressourceController.create(_, response, next, Player);
+}
+
+playerController.update = async(_, response, next) => {
+    console.log('enter playerController.update');
+    ressourceController.update(_, response, next, Player);
+}
+
+playerController.destroy = async(_, response, next) => {
+    console.log('enter playerController.destroy');
+    ressourceController.destroy(_, response, next, Player);
+}
+
 module.exports = playerController;
