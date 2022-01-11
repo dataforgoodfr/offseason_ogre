77a51472bc8442759781a9f39353baa2f33b883d
TCateland <77750894+TCateland@users.noreply.github.com>
2022-01-11 16:26:05 +0100

    Model modification and constants addition
    Modification of Model to fit with sequelize and addition of the constatnts in OGREConstants file

--- server/app/OGREConstants.js
+++ server/app/OGREConstants.js
@@ -1,3 +1,26 @@
+// Base calcul conso carburant par passager (Boeing 747-400) pour 1 A/R
+const planeConstants = {
+    consumptionOneRide : 24000,
+    distanceOneRide : 14200,
+    passengersByPlane : 416,
+    calorificValue : 10,
+    get consumptionByPassenger() {
+        return (this.consumptionOneRide * this.calorificValue / this.passengersByPlane) * 2
+    }
+}
+
+const windTurbineOnshoreConstants = {
+    LoadFactor : 0.33,
+    CO2EmissionsPerkWh : 10,
+    LCOEaverage : 0.0605,
+    PowerM2 : 2
+}
+
+const GlobalConstants = {
+    PopulationFrance : 64810000,
+    AreaFrance : 551695
+}
+
 const OGREConstants = {
     CONSUMPTION100KM: 7,
     CALORIFIC_VALUE: 10,
@@ -5,6 +28,8 @@ const OGREConstants = {
         DIESEL: 'Diesel',
         ELECTRIQUE: 'Electrique',
         HYBRIDE: 'Hybride'
-    }
+    },
+    planeConstants : planeConstants,
+    daysPerYear : 365
 }
 module.exports = OGREConstants
