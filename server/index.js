828966fb9ae3c25eb70eeed4699826bb790e33d3
Vladimir <vladimir@DESKTOP-GC7CN5C.localdomain>
2022-01-18 15:29:59 +0100

    allowed body parsing (use of req.body) with express body-parser

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ const express = require('express');
 const app = express();
 
 // Parse URL-encoded bodies (as sent by HTML forms)
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.urlencoded({ extended: false }));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 // Parse JSON bodies (as sent by API clients)
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 //parse cookies sent via http requests
 // const cookieParser = require('cookie-parser');
-// app.use(cookieParser()) 
+// app.use(cookieParser())
 
 //use cors to accept other non domain websites to access api
 // const cors = require('cors');
