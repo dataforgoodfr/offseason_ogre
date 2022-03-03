c21f405f80387e8d8454dd574559416f0dfe6253
Vladimir <vladimir@DESKTOP-GC7CN5C.localdomain>
2022-03-03 11:39:34 +0100

    added ssl true to sequelize connection for heroku

--- server/app/config/database.js
+++ server/app/config/database.js
@@ -3,6 +3,9 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     define: {
         underscored: true, //camelCase fields in javascript are translated to snace_case fields in database tables
+    },
+    dialectOptions: {
+        ssl: true
     }
 });
 
