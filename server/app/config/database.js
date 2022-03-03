7dd81a477e6a28fb0b1335ff7c6ad6162a59fb55
Vladimir <vladimir@DESKTOP-GC7CN5C.localdomain>
2022-03-03 14:22:47 +0100

    test ssl 4

--- server/app/config/database.js
+++ server/app/config/database.js
@@ -6,10 +6,11 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
     ssl:true,
     dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-        }
+        ssl: true,
+        // ssl: {
+        //   require: true,
+        //   rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+        // }
     },
 });
 
