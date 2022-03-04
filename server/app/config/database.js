5d46d2948989fb3ad3712bb6c7910c35535fd183
Vladimir <vladimir@DESKTOP-GC7CN5C.localdomain>
2022-03-04 15:39:04 +0100

    ssl try

--- server/app/config/database.js
+++ server/app/config/database.js
@@ -4,13 +4,14 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     define: {
         underscored: true, //camelCase fields in javascript are translated to snace_case fields in database tables
     },
-    ssl:true,
+    // ssl:true,
+    //native: true,
     dialectOptions: {
-        //ssl: true,
-        ssl: {
-          require: true,
-          rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-        }
+        ssl: true,
+        // ssl: {
+        //   require: true,
+        //   //rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+        // }
     },
 });
 
