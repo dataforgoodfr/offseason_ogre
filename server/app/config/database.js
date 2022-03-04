3e728b609a8f0cf163b4578ab3fb5904dfa9c61d
Vladimir <vladimir@DESKTOP-GC7CN5C.localdomain>
2022-03-04 15:55:57 +0100

    test ssl 5

--- server/app/config/database.js
+++ server/app/config/database.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(`${process.env.DATABASE_URL}?sslmode=require`, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    // `${process.env.DATABASE_URL}?sslmode=require`
     define: {
         underscored: true, //camelCase fields in javascript are translated to snace_case fields in database tables
     },
