41d6bff781d2b0e426f2991b2bb227824b29475b
Vladimir <vladimir@DESKTOP-GC7CN5C.localdomain>
2022-03-04 16:18:31 +0100

    failure omg

--- server/app/config/database.js
+++ server/app/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(`${process.env.DATABASE_URL}?sslmode=require`, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     // `${process.env.DATABASE_URL}?sslmode=require`
     define: {
         underscored: true, //camelCase fields in javascript are translated to snace_case fields in database tables
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(`${process.env.DATABASE_URL}?sslmode=require`, {
     // ssl:true,
     // native: true,
     dialectOptions: {
-        ssl: true,
+        // ssl: true,
         // ssl: {
         //   //require: true,
         //   rejectUnauthorized: false // <<<<<<< YOU NEED THIS
