29b8be8d612630acfaebb77c23e2effe92fbbecb
Ibrahim <abram.pomposelli1@gmail.com>
2022-03-25 00:55:08 +0100

    husky test 2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import TestModal from "./components/TestModal";
 import Home from "./components/Home";
 
-console.log("su");
 function App() {
   return (
     <div className="App">
