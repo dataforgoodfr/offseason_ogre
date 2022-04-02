d9fcd30b0d85954ed13556467ce9069c9f698881
louis-sanna-eki <louis.sanna@gmail.com>
2022-04-02 19:27:51 +0200

    Add script at root

--- packages/server/src/modules/utils/crudBuilders.ts
+++ packages/server/src/modules/utils/crudBuilders.ts
@@ -1,16 +1,16 @@
-import type { Model } from 'sequelize';
-import type { Request, Response } from 'express';
-import * as z from 'zod';
+import type { Model } from "sequelize";
+import type { Request, Response } from "express";
+import * as z from "zod";
 
 export { buildCrudControllers, buildCrudServices };
 export type { CrudControllers, CrudServices };
 
 interface CrudServices<T> {
-    getDocument: (id: number) => Promise<T> 
+  getDocument: (id: number) => Promise<T>;
 }
 
 interface CrudControllers<T> {
-    getDocumentController: (request: Request, response: Response) => void
+  getDocumentController: (request: Request, response: Response) => void;
 }
 
 // Typing is badly done, but I don't want to spend more type unstanding sequelize typing
@@ -18,29 +18,32 @@ interface CrudControllers<T> {
 type Constructor<M> = new (...args: any[]) => M;
 type ModelType<M extends Model<M>> = Constructor<M> & typeof Model;
 
-function buildCrudServices<T, M extends Model<M>>(model: ModelType<M>): CrudServices<T> {
-
-    const getDocument = async (id: number): Promise<T> => {
-        const document = model.findByPk(id) as unknown as T | null;
-        if (document === null) {
-            throw new Error("Not found!");
-        };
-        return document;
+function buildCrudServices<T, M extends Model<M>>(
+  model: ModelType<M>
+): CrudServices<T> {
+  const getDocument = async (id: number): Promise<T> => {
+    const document = model.findByPk(id) as unknown as T | null;
+    if (document === null) {
+      throw new Error("Not found!");
     }
+    return document;
+  };
 
-    return {
-        getDocument
-    }
+  return {
+    getDocument,
+  };
 }
 
-function buildCrudControllers<T>(services: CrudServices<T>): CrudControllers<T> {
-    const getDocumentController = async(req, res) => {
-        const paramsSchema = z.object({
-            id: z.string().regex(/^\d+$/).transform(Number),
-        });
-        const { id } = paramsSchema.parse(req.params);
-        const document = await services.getDocument(id)
-        res.status(200).json({ data: document });
-    };
-    return { getDocumentController };
+function buildCrudControllers<T>(
+  services: CrudServices<T>
+): CrudControllers<T> {
+  const getDocumentController = async (req, res) => {
+    const paramsSchema = z.object({
+      id: z.string().regex(/^\d+$/).transform(Number),
+    });
+    const { id } = paramsSchema.parse(req.params);
+    const document = await services.getDocument(id);
+    res.status(200).json({ data: document });
+  };
+  return { getDocumentController };
 }
