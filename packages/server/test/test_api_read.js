44b4a9675a0e0a22a8eed7c54d18203c25c2acb0
Ibrahim <abram.pomposelli1@gmail.com>
2022-04-06 13:30:41 +0200

    Add packages folder, remove craco

---
+++ packages/server/test/test_api_read.js
@@ -0,0 +1,100 @@
+const should = require("should");
+const request = require("request");
+const expect = require("chai").expect;
+// const baseUrl = "http://localhost:8080/";
+const baseUrl = "https://atelierogre-staging.herokuapp.com/";
+const util = require("util");
+const id = 1;
+const maxChildren = 10;
+const distancePerYear = 200000;
+
+/*********************** tests player ***********************************/
+
+describe("players tests", function () {
+  it("should return status", function (done) {
+    request.get(
+      { url: baseUrl + "players/" + id },
+      function (error, response, body) {
+        const bodyObj = JSON.parse(body);
+        expect(response.statusCode).to.equal(200);
+        done();
+      }
+    );
+  });
+  it("should assert id number", function (done) {
+    request.get(
+      { url: baseUrl + "players/" + id },
+      function (error, response, body) {
+        const bodyObj = JSON.parse(body);
+        expect(bodyObj.data.id).to.equal(id);
+        expect(bodyObj.data.id).to.be.a("number");
+        done();
+      }
+    );
+  });
+  it("should assert max children number", function (done) {
+    request.get(
+      { url: baseUrl + "players/" + id },
+      function (error, response, body) {
+        const bodyObj = JSON.parse(body);
+        expect(bodyObj.data.id).to.below(maxChildren);
+        done();
+      }
+    );
+  });
+  it("should assert distance per year", function (done) {
+    request.get(
+      { url: baseUrl + "players/" + id },
+      function (error, response, body) {
+        const bodyObj = JSON.parse(body);
+        expect(bodyObj.data.id).to.below(distancePerYear);
+        done();
+      }
+    );
+  });
+});
+
+/*********************** tests car consumptions *************************/
+
+describe("car consumptions tests", function () {
+  it("should return status", function (done) {
+    request.get(
+      { url: baseUrl + "carconsumptions/" + id },
+      function (error, response, body) {
+        const bodyObj = JSON.parse(body);
+        expect(response.statusCode).to.equal(200);
+        done();
+      }
+    );
+  });
+});
+
+/*********************** tests plane consumptions ***********************/
+
+describe("plane consumptions tests", function () {
+  it("should return status", function (done) {
+    request.get(
+      { url: baseUrl + "planeconsumptions/" + id },
+      function (error, response, body) {
+        const bodyObj = JSON.parse(body);
+        expect(response.statusCode).to.equal(200);
+        done();
+      }
+    );
+  });
+});
+
+/*********************** tests wind turbine consumptions ****************/
+
+describe("wind turbine consumptions tests", function () {
+  it("should return status", function (done) {
+    request.get(
+      { url: baseUrl + "planeconsumptions/" + id },
+      function (error, response, body) {
+        const bodyObj = JSON.parse(body);
+        expect(response.statusCode).to.equal(200);
+        done();
+      }
+    );
+  });
+});
--- server/app/tests/models/Player.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require("assert").strict;
-const Player = require('../../db/models/Player')
-
-describe("test nb of persons per house for a Player ", () => {
-    it('Should compute in case: 2 adults and 3 childrens', async() => {
-        const player = await Player.findByPk(1)
-        assert.equal(player.personsPerHousehold, player.adultsPerHousehold + player.childrenPerHousehold)
-    });
-});
