3cd23e2cc14d04af62131aef35391c5520d1e47c
louis-sanna-eki <louis.sanna@gmail.com>
2022-04-14 21:33:34 +0200

    Remove useless config

--- packages/client/.eslintrc.ts
+++ packages/client/.eslintrc.ts
@@ -3,19 +3,11 @@ const config = {
     browser: true,
     jest: true,
   },
-  extends: ["airbnb-typescript", "react-app"], // if you're using typescript
+  extends: ["airbnb-typescript", "react-app"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.json",
   },
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"], // Your TypeScript files extension
-      parserOptions: {
-        project: ["./tsconfig.json"], // Specify it only for TypeScript files
-      },
-    },
-  ],
 };
 
 export default config;
