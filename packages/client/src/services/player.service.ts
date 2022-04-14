0b908d1b4df4d19e0e6cc1fd5a20e6579c9b84a3
louis-sanna-eki <louis.sanna@gmail.com>
2022-04-14 21:17:59 +0200

    Add prettier on front

--- packages/client/src/services/player.service.ts
+++ packages/client/src/services/player.service.ts
@@ -2,29 +2,29 @@ import http from "../http-common";
 import IPlayerData from "../types/player.type";
 
 // CRUD operations for player data
-class PlayerDataService
-{
+class PlayerDataService {
   getAll() {
-    return http.get<Array<IPlayerData>>("/players")
+    return http.get<Array<IPlayerData>>("/players");
   }
 
-  get(id : number) {
-    return http.get<IPlayerData>(`players/${id}`)
+  get(id: number) {
+    return http.get<IPlayerData>(`players/${id}`);
   }
 
-  create(data : IPlayerData) {
-    return http.put<IPlayerData>("players", data)
+  create(data: IPlayerData) {
+    return http.put<IPlayerData>("players", data);
   }
 
-  update(data : IPlayerData, id : number) {
-    return http.post<any>(`players/${id}`, data)
+  update(data: IPlayerData, id: number) {
+    return http.post<any>(`players/${id}`, data);
   }
 
-  delete(id : number) {
-    return http.delete<any>(`players/${id}`)
+  delete(id: number) {
+    return http.delete<any>(`players/${id}`);
   }
 
   deleteAll() {
     return http.delete<any>(`players`);
   }
-} export default new PlayerDataService();
+}
+export default new PlayerDataService();
