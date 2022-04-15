b3d990524cd5498a70cbfaf7fa7ec44ec97c8e65
louis-sanna-eki <louis.sanna@gmail.com>
2022-04-15 19:27:08 +0200

    Add mui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,7 @@
-import logo from "./logo.svg";
-import PlayerSelect from "./components/PlayerSelect";
+import { Button } from "@mui/material";
 
 function App() {
-  return (
-    <div className="App bg-slate-800 h-screen">
-      <header className="App-header space-y-60 flex flex-col items-center">
-        <img
-          src={logo}
-          className="App-logo max-w-3xl max-h-3xl animate-[spin_4s_linear_infinite]"
-          alt="logo"
-        />
-        <PlayerSelect />
-      </header>
-    </div>
-  );
+  return <Button variant="contained">Hello World!</Button>;
 }
 
 export default App;
