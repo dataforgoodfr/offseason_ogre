421c3d4b0a7a534bbc7ca9ec9fbeae4200d88d93
louis-sanna-eki <louis.sanna@gmail.com>
2022-04-23 15:19:57 +0200

    Fix typing

--- packages/server/src/database/seed.ts
+++ packages/server/src/database/seed.ts
@@ -1,15 +1,25 @@
 import { prisma } from ".";
 
+// eslint-disable-next-line no-console
 console.log("Starting seeding...");
 
 async function seed() {
   await prisma.$connect();
 
-  await prisma.user.create({ data: { email: "seeding@database.com" } });
+  await prisma.user.create({
+    data: {
+      email: "seeding@database.com",
+      isTeacher: false,
+      firstName: "Seeding",
+      lastName: "Master",
+      country: "FR",
+    },
+  });
 
   await prisma.$disconnect();
 }
 
 seed().then(() => {
+  // eslint-disable-next-line no-console
   console.log("Seeding done");
 });
