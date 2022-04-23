0b96320ca5ecb27918873e9c5e929cae61deee79
louis-sanna-eki <louis.sanna@gmail.com>
2022-04-23 13:27:13 +0200

    Create user POST route

--- packages/server/src/modules/users/controllers/index.ts
+++ packages/server/src/modules/users/controllers/index.ts
@@ -1,7 +1,26 @@
+import type { Request, Response } from "express";
+import { z } from "zod";
 import { services } from "../services";
-import { buildCrudControllers } from "../../utils/crudBuilders";
 
-const crudController = buildCrudControllers(services);
+const crudController = { getDocumentController, createController };
 const controllers = { ...crudController };
 
 export { controllers };
+
+async function createController(request: Request, response: Response) {
+  const bodySchema = z.object({
+    email: z.string().email(),
+  });
+  const documentToCreate = bodySchema.parse(request.body);
+  const newDocument = await services.create(documentToCreate);
+  response.status(201).json({ data: newDocument });
+}
+
+async function getDocumentController(request: Request, response: Response) {
+  const paramsSchema = z.object({
+    id: z.string().regex(/^\d+$/).transform(Number),
+  });
+  const { id } = paramsSchema.parse(request.params);
+  const document = await services.getDocument(id);
+  response.status(200).json({ data: document });
+}
--- packages/server/src/modules/users/services/index.ts
+++ packages/server/src/modules/users/services/index.ts
@@ -1,9 +1,18 @@
 import { prisma } from "../../../database";
-import { buildCrudServices } from "../../utils/crudBuilders";
 import { User } from "../types/entity";
 
 const model = prisma.user;
-const crudServices = buildCrudServices<User>(model);
+type Model = User;
+
+const crudServices = { getDocument, create };
 const services = { ...crudServices };
 
 export { services };
+
+async function getDocument(id: number): Promise<Model | null> {
+  return model.findUnique({ where: { id } });
+}
+
+async function create(document: Omit<Model, "id">): Promise<Model> {
+  return model.create({ data: document });
+}
