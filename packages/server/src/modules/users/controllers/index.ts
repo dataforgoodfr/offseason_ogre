946a8078bab8738cbfe12f42acefb2e865ce4250
felix barriere <felixbarriere@LAPTOP-GTUTFUJ1.localdomain>
2022-04-26 12:38:17 +0200

    commit to checkout

--- packages/server/src/modules/users/controllers/index.ts
+++ packages/server/src/modules/users/controllers/index.ts
@@ -8,34 +8,33 @@ const controllers = { ...crudController };
 export { controllers };
 
 async function createController(request: Request, response: Response) {
-  const bodySchema = z.object({
-    country: z.string(),
-    email: z.string(),
-    lastName: z.string(),
-    firstName: z.string(),
-  });
-  const documentToCreate = bodySchema.parse(request.body);
-  const newDocument = await services.create({
-    ...documentToCreate,
-    isTeacher: false,
-  });
-  response.status(201).json({ data: newDocument });
+	const bodySchema = z.object({
+		country: z.string(),
+		email: z.string(),
+		lastName: z.string(),
+		firstName: z.string(),
+	});
+	const documentToCreate = bodySchema.parse(request.body);
+	const newDocument = await services.create({
+		...documentToCreate,
+		isTeacher: false,
+	});
+	response.status(201).json({ data: newDocument });
 }
 
 async function getDocumentController(request: Request, response: Response) {
-  const paramsSchema = z.object({
-    id: z.string().regex(/^\d+$/).transform(Number),
-  });
-  const { id } = paramsSchema.parse(request.params);
-  const document = await services.getDocument(id);
-  response.status(200).json({ data: document });
+	const paramsSchema = z.object({
+		id: z.string().regex(/^\d+$/).transform(Number),
+	});
+	const { id } = paramsSchema.parse(request.params);
+	const document = await services.getDocument(id);
+	response.status(200).json({ data: document });
 }
 
 async function signIn(request: Request, response: Response) {
-  const email = await services.findEmail(request.body.email);
-  if (email) {
-    response.status(200).send("email existant");
-  } else {
-    response.status(200).send("email non existant");
-  }
+	if (await services.isMailAlreadyUsed(request.body.email) == 1) {
+		response.status(200).send("email existant!");
+	} else {
+		response.status(200).send("email non existant!");
+	}
 }
--- packages/server/src/modules/users/services/index.ts
+++ packages/server/src/modules/users/services/index.ts
@@ -1,22 +1,52 @@
 import { prisma } from "../../../database";
 import { User } from "../types/entity";
+// const jwt = require("jsonwebtoken");
+// const sendGridMail = require("@sendgrid/mail");  utiliser import
 
 const model = prisma.user;
 type Model = User;
 
-const crudServices = { getDocument, create, findEmail };
+const crudServices = { getDocument, create, isMailAlreadyUsed };
 const services = { ...crudServices };
 
 export { services };
 
 async function getDocument(id: number): Promise<Model | null> {
-  return model.findUnique({ where: { id } });
+	return model.findUnique({ where: { id } });
 }
 
 async function create(document: Omit<Model, "id">): Promise<Model> {
-  return model.create({ data: document });
+	return model.create({ data: document });
 }
 
-async function findEmail(email: string): Promise<Model | null> {
-  return model.findUnique({ where: { email } });
+async function isMailAlreadyUsed(email: string): Promise<number | null> {
+	const mail = await model.findUnique({ where: { email } });
+	if (mail) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+const generate = (email) => {
+	return jwt.sign({ email }, "secret_key", { expiresIn: "60" });
+};
+
+async function sendWithSendgrid(email: String) {
+	const url = "https://atelierogre-staging.herokuapp.com/";
+	const token = generate(email);
+
+	sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+	const sendMail = async (msg) => {
+		await sendGridMail.send(msg);
+	};
+
+	sendMail({
+		to: "felix.barriere@gmail.com",
+		from: "felix.barriere@gmail.com",
+		subject: "Votre lien de connexion OGRE",
+		text: "Votre lien: ",
+		html: `<p><a href="${url}"> account?token=${token} </a></p>`,
+	});
 }
