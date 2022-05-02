1cdaac96ec8ba0ce889e662bcfda03026dcb35b9
louis-sanna-eki <louis.sanna@gmail.com>
2022-05-02 15:10:54 +0200

    Extract unauth code

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,28 +1,7 @@
-import { ThemeProvider } from "@mui/material";
-import { Routes, Route } from "react-router-dom";
-import OgreHeader from "./modules/common/components/OgreHeader";
-import MagicLink from "./modules/magic-link";
-import Signup from "./modules/signup";
-import { theme } from "./utils/theme";
+import { UnauthenticatedApp } from "./UnauthenticatedApp";
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <div
-        className="min-h-screen flex flex-col"
-        style={{ backgroundColor: theme.palette.primary.main }}
-      >
-        <OgreHeader />
-        <div className="flex flex-col min-h-[80vh] items-center">
-          <Routes>
-            <Route path="/" element={<MagicLink />} />
-            <Route path="/magic-link" element={<MagicLink />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </div>
-      </div>
-    </ThemeProvider>
-  );
+  return <UnauthenticatedApp></UnauthenticatedApp>;
 }
 
 export default App;
