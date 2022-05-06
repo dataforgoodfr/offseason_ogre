08615424289232eec0695e816e5ff32eb0674851
louis-sanna-eki <louis.sanna@gmail.com>
2022-05-06 16:42:37 +0200

    Add react query

--- packages/client/src/modules/administration/NewGame.tsx
+++ packages/client/src/modules/administration/NewGame.tsx
@@ -1,22 +1,49 @@
-import { Box, Button, Grid, TextField, Typography } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  Grid,
+  Snackbar,
+  TextField,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
+import { useMutation } from "react-query";
 import { Layout } from "./Layout";
 
 export { NewGame };
 
+interface INewGame {
+  name: string;
+}
+
 function NewGame(): JSX.Element {
-  const { control } = useForm({
+  const { control, handleSubmit } = useForm({
     defaultValues: {
       name: "",
     },
   });
+
+  const mutation = useMutation<Response, { message: string }, INewGame>(
+    (newGame) => {
+      return axios.post("/api/games", newGame);
+    }
+  );
+
+  const onSubmit = (newGame: INewGame) => {
+    return mutation.mutate(newGame);
+  };
+
+  console.log("mutation.isError", mutation.isError);
 
   return (
     <Layout>
       <>
         <Typography variant="h3">New Game</Typography>
         <Box sx={{ mt: 4 }}>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <Grid container direction="column">
               <Controller
                 control={control}
@@ -41,7 +68,25 @@ function NewGame(): JSX.Element {
             </Grid>
           </form>
         </Box>
+        {mutation.isError && <ErrorAlert message={mutation.error.message} />}
       </>
     </Layout>
+  );
+}
+
+function ErrorAlert({ message }: { message: string }) {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const onClose = () => setIsOpen(false);
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      autoHideDuration={6000}
+      onClose={onClose}
+      open={isOpen}
+    >
+      <Alert onClose={onClose} severity="error" variant="filled">
+        {message}
+      </Alert>
+    </Snackbar>
   );
 }
