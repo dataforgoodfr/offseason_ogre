c799d05e0118c2be955d5fdbc6e45afa54d1efad
Remi <remi.riviere@marcel.cab>
2022-05-12 17:31:41 +0200

    prettier

--- packages/server/src/modules/games/controllers/types.ts
+++ packages/server/src/modules/games/controllers/types.ts
@@ -1,3 +1,7 @@
 import { z } from "zod";
 
-export const dateSchema = z.preprocess((arg) => (typeof arg === "string" || arg instanceof Date) ? new Date(arg) : arg, z.date());
+export const dateSchema = z.preprocess(
+  (arg) =>
+    typeof arg === "string" || arg instanceof Date ? new Date(arg) : arg,
+  z.date()
+);
