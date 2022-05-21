b9fb04c0d3b8a2461ed5b969efb6b57a1db897d2
louis-sanna-eki <louis.sanna@gmail.com>
2022-05-21 16:02:22 +0200

    Give teacher rights to ogre team members

--- packages/server/src/modules/users/services/index.ts
+++ packages/server/src/modules/users/services/index.ts
@@ -19,9 +19,31 @@ async function getDocument(id: number): Promise<Model | null> {
   return model.findUnique({ where: { id } });
 }
 
-async function create(document: Omit<Model, "id">): Promise<Model> {
-  if (await isMailAlreadyUsed(document.email)) {
+async function create(newUser: Omit<Model, "id">): Promise<Model> {
+  if (await isMailAlreadyUsed(newUser.email)) {
     throw new Error("Email is already taken.");
   }
-  return model.create({ data: document });
+  return model.create({
+    data: { ...newUser, isTeacher: computeIsTeacher(newUser) },
+  });
+}
+
+function computeIsTeacher(newUser: Omit<Model, "id">) {
+  if (shouldHaveTeacherRole(newUser.email)) {
+    return true;
+  }
+  return newUser.isTeacher ?? false;
+}
+
+function shouldHaveTeacherRole(email: string): boolean {
+  const mails = [
+    "b00461284@essec.edu",
+    "felix.barriere@gmail.com",
+    "grandeur.energies@gmail.com",
+    "louis.sanna@gmail.com",
+    "ogre.teacher@yopmail.com",
+    "remi.riviere.free@gmail.com",
+    "tcateland@gmail.com",
+  ];
+  return mails.includes(email);
 }
