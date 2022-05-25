18c0c9ff0328e07a3b5783bbd890601170775af7
Vladimir Nafissi <vlad.nafissi@gmail.com>
2022-05-25 20:04:51 +0200

    ajout msg warning in dialog modal if a team has more than 5 players

--- packages/client/src/modules/games/Game/Animation/Launch.tsx
+++ packages/client/src/modules/games/Game/Animation/Launch.tsx
@@ -9,10 +9,27 @@ import {
 import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
 import { useState } from "react";
 import axios from "axios";
-import { useMutation, useQueryClient } from "react-query";
+import { useMutation, useQueryClient, useQuery } from "react-query";
 import { IGame } from "../../../../utils/types";
 import { SuccessAlert } from "../../../alert";
 import { Navigate } from "react-router-dom";
+
+const TeamHasTooManyPLayers = (playersQuery: any) => {
+  const players = playersQuery?.data?.data?.players ?? [];
+  const teams = players.map((player: any) => (
+    player.playedGames[0].team.name
+  ));
+  const teamOccurences = teams.reduce((obj: any, teamName: string) => {
+    obj[teamName] = ++obj[teamName] || 1;
+    return obj;
+  }, {});
+  for (let teamName in teamOccurences) {
+    if (teamOccurences[teamName] > 5) {
+      return true;
+    }
+  }
+  return false;
+}
 
 export default function Launch({ game }: { game: IGame }) {
   const queryClient = useQueryClient();
@@ -20,6 +37,15 @@ export default function Launch({ game }: { game: IGame }) {
   const [open, setOpen] = useState(false);
   const handleClickOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const playersQuery = useQuery(`/api/games/${game.id}/players`, () => {
+    return axios.get<undefined, { data: { players: any[] } }>(
+      `/api/games/${game.id}/players`
+    );
+  });
+  const dialogContent = {
+    message: "Êtes-vous sûr.e de vouloir lancer l'atelier ?",
+    warningMessage: "Une ou plusieurs équipe(s) dépasse(nt) le nombre de joueurs recommandé (5 joueurs)"
+  }
 
   const mutation = useMutation<Response, { message: string }, any>(
     (status: boolean) => {
@@ -59,8 +85,14 @@ export default function Launch({ game }: { game: IGame }) {
           {"Lancer la partie ?"}
         </DialogTitle>
         <DialogContent>
+          <DialogContentText
+            id="alert-dialog-warning"
+            sx={{ color: 'red', mb: 2 }}
+          >
+            {TeamHasTooManyPLayers(playersQuery) && dialogContent.warningMessage}
+          </DialogContentText>
           <DialogContentText id="alert-dialog-description">
-            Êtes-vous sûr.e de vouloir lancer l'atelier ?
+            {dialogContent.message}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
