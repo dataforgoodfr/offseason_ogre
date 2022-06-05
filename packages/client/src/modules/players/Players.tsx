a616d714fa73dc6dc1271c4bcfcff11c6e225fec
Baboo7 <dbsharp404@gmail.com>
2022-06-05 14:32:39 +0200

    feat(Players): Update player role from admin

--- packages/client/src/modules/players/Players.tsx
+++ packages/client/src/modules/players/Players.tsx
@@ -1,9 +1,19 @@
+import ArrowDropDown from "@mui/icons-material/ArrowDropDown";
 import { Box, CircularProgress, Paper, Typography } from "@mui/material";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import {
+  DataGrid,
+  GridRenderCellParams,
+  GridColDef,
+  useGridApiContext,
+} from "@mui/x-data-grid";
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 
+import { IUser } from "../../utils/types";
 import { Layout } from "../administration/Layout";
+import { SuccessAlert } from "../alert";
 
 export { Players };
 
@@ -21,6 +31,45 @@ function Players(): JSX.Element {
     </Layout>
   );
 }
+
+function renderPlayerRoleCell(props: any) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <span>{props.value ? "Animateur" : "Joueur"}</span>
+      <ArrowDropDown sx={{ fontSize: "1.2rem" }} />
+    </Box>
+  );
+}
+
+function PlayerRoleEditInputCell(props: GridRenderCellParams<boolean>) {
+  const { id, value, field } = props;
+  const apiRef = useGridApiContext();
+
+  const handleChange = (event: SelectChangeEvent) => {
+    const newValue = event.target.value === "true";
+    apiRef.current.setEditCellValue({ id, field, value: newValue });
+  };
+
+  return (
+    <Box sx={{ display: "flex", flex: 1, alignItems: "center" }}>
+      <Select sx={{ flex: 1 }} value={String(value)} onChange={handleChange}>
+        <MenuItem value="true">Animateur</MenuItem>
+        <MenuItem value="false">Joueur</MenuItem>
+      </Select>
+    </Box>
+  );
+}
+
+const renderPlayerRoleEditInputCell: GridColDef["renderCell"] = (params) => {
+  return <PlayerRoleEditInputCell {...params} />;
+};
 
 const columns: GridColDef<{
   lastName: string;
@@ -53,37 +102,53 @@ const columns: GridColDef<{
   {
     field: "isTeacher",
     headerName: "Rôle",
+    editable: true,
+    type: "boolean",
     valueGetter: (params) => params.row.isTeacher,
-    renderCell: function (props) {
-      return props.value ? "Animateur" : "Joueur";
-    },
+    renderCell: renderPlayerRoleCell,
+    renderEditCell: renderPlayerRoleEditInputCell,
     flex: 1,
     minWidth: 150,
   },
 ];
 
 function UsersDataGrid() {
-  const query = useQuery("users", () => {
+  const queryUsers = useQuery("users", () => {
     return axios.get<undefined, { data: { documents: any[] } }>(
       "/api/users?page=1&sort=email:asc"
     );
   });
 
-  if (query.isLoading) {
+  const mutateUser = useMutation((user: IUser) => {
+    const path = `/api/users/${user.id}`;
+    return axios.put(path, user);
+  });
+
+  const handleCellEditCommit = (params: any) => {
+    const newValue = { ...params.row, [params.field]: params.value };
+    mutateUser.mutate(newValue);
+  };
+
+  if (queryUsers.isLoading) {
     return <CircularProgress />;
   }
 
-  const rows = query?.data?.data?.documents ?? [];
+  const rows = queryUsers?.data?.data?.documents ?? [];
 
   return (
-    <Box style={{ height: 600, width: "100%", cursor: "pointer" }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        disableSelectionOnClick
-        pageSize={20}
-        rowsPerPageOptions={[20]}
-      />
-    </Box>
+    <>
+      {mutateUser.isSuccess && <SuccessAlert />}
+
+      <Box style={{ height: 600, width: "100%", cursor: "pointer" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          disableSelectionOnClick
+          pageSize={20}
+          rowsPerPageOptions={[20]}
+          onCellEditCommit={handleCellEditCommit}
+        />
+      </Box>
+    </>
   );
 }
