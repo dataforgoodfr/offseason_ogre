7035d4184868b752043b210738b91b760ddb283b
louis-sanna-eki <louis.sanna@gmail.com>
2022-06-06 19:51:36 +0200

    Use default boolean update

--- packages/client/src/modules/players/Players.tsx
+++ packages/client/src/modules/players/Players.tsx
@@ -1,13 +1,5 @@
-import ArrowDropDown from "@mui/icons-material/ArrowDropDown";
 import { Box, CircularProgress, Paper, Typography } from "@mui/material";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import {
-  DataGrid,
-  GridRenderCellParams,
-  GridColDef,
-  useGridApiContext,
-} from "@mui/x-data-grid";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import axios from "axios";
 import { useMutation, useQuery } from "react-query";
 
@@ -31,45 +23,6 @@ function Players(): JSX.Element {
     </Layout>
   );
 }
-
-function renderPlayerRoleCell(props: any) {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
-      <span>{props.value ? "Animateur" : "Joueur"}</span>
-      <ArrowDropDown sx={{ fontSize: "1.2rem" }} />
-    </Box>
-  );
-}
-
-function PlayerRoleEditInputCell(props: GridRenderCellParams<boolean>) {
-  const { id, value, field } = props;
-  const apiRef = useGridApiContext();
-
-  const handleChange = (event: SelectChangeEvent) => {
-    const newValue = event.target.value === "true";
-    apiRef.current.setEditCellValue({ id, field, value: newValue });
-  };
-
-  return (
-    <Box sx={{ display: "flex", flex: 1, alignItems: "center" }}>
-      <Select sx={{ flex: 1 }} value={String(value)} onChange={handleChange}>
-        <MenuItem value="true">Animateur</MenuItem>
-        <MenuItem value="false">Joueur</MenuItem>
-      </Select>
-    </Box>
-  );
-}
-
-const renderPlayerRoleEditInputCell: GridColDef["renderCell"] = (params) => {
-  return <PlayerRoleEditInputCell {...params} />;
-};
 
 const columns: GridColDef<{
   lastName: string;
@@ -101,12 +54,10 @@ const columns: GridColDef<{
   },
   {
     field: "isTeacher",
-    headerName: "Rôle",
+    headerName: "Animateur",
     editable: true,
     type: "boolean",
     valueGetter: (params) => params.row.isTeacher,
-    renderCell: renderPlayerRoleCell,
-    renderEditCell: renderPlayerRoleEditInputCell,
     flex: 1,
     minWidth: 150,
   },
