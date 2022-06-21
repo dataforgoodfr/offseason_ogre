579c0fa037678d819c8d94f52f82013d3691d3ab
Vladimir Nafissi <vlad.nafissi@gmail.com>
2022-06-21 17:50:07 +0200

    linter

--- packages/client/src/modules/play/playerActions/playerActions.tsx
+++ packages/client/src/modules/play/playerActions/playerActions.tsx
@@ -7,34 +7,34 @@ import { PlayerHeader } from "../PlayerPersona/PlayerHeader";
 export { PlayerActions, PlayerHeader };
 
 function PlayerActions() {
-    return (
-        <PlayLayout title="Persona">
-            <Box>
-                <Grid container direction="row">
-                    <Grid
-                        item
-                        xs={12}
-                        sm={3}
-                        sx={{
-                            pl: 1,
-                            pr: 1,
-                        }}
-                    >
-                        <PlayerHeader />
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        sm={9}
-                        sx={{
-                            pl: 1,
-                            pr: 1,
-                        }}
-                    >
-                        <Actions />
-                    </Grid>
-                </Grid>
-            </Box>
-        </PlayLayout>
-    );
+  return (
+    <PlayLayout title="Persona">
+      <Box>
+        <Grid container direction="row">
+          <Grid
+            item
+            xs={12}
+            sm={3}
+            sx={{
+              pl: 1,
+              pr: 1,
+            }}
+          >
+            <PlayerHeader />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={9}
+            sx={{
+              pl: 1,
+              pr: 1,
+            }}
+          >
+            <Actions />
+          </Grid>
+        </Grid>
+      </Box>
+    </PlayLayout>
+  );
 }
