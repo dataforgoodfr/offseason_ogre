a539aa235d5fca1bb5cece2f329f937f5744a147
Vladimir Nafissi <vlad.nafissi@gmail.com>
2022-06-21 17:59:02 +0200

    prettier

--- packages/client/src/modules/play/playerActions/Actions.tsx
+++ packages/client/src/modules/play/playerActions/Actions.tsx
@@ -12,70 +12,70 @@ import { ActionsHeader } from "./ActionsHeader";
 export { Actions };
 
 function Actions() {
-    return (
-        <PlayBox>
-            <ActionsHeader />
-            <ActionLayout title="Passer au véhicule électrique">
-                <Typography>Caractéristiques.</Typography>
-            </ActionLayout>
-            <ActionLayout title="Déplacement en voiture">
-                <Typography>Caractéristiques.</Typography>
-            </ActionLayout>
-            <ActionLayout title="Moins d'équipement numérique">
-                <Typography>Caractéristiques.</Typography>
-            </ActionLayout>
-            <ActionLayout title="Arrêt boissons canettes">
-                <Typography>Caractéristiques.</Typography>
-            </ActionLayout>
-            <ActionLayout title="Acheter moins de vêtements">
-                <Typography>Caractéristiques.</Typography>
-            </ActionLayout>
-        </PlayBox>
-    );
+  return (
+    <PlayBox>
+      <ActionsHeader />
+      <ActionLayout title="Passer au véhicule électrique">
+        <Typography>Caractéristiques.</Typography>
+      </ActionLayout>
+      <ActionLayout title="Déplacement en voiture">
+        <Typography>Caractéristiques.</Typography>
+      </ActionLayout>
+      <ActionLayout title="Moins d'équipement numérique">
+        <Typography>Caractéristiques.</Typography>
+      </ActionLayout>
+      <ActionLayout title="Arrêt boissons canettes">
+        <Typography>Caractéristiques.</Typography>
+      </ActionLayout>
+      <ActionLayout title="Acheter moins de vêtements">
+        <Typography>Caractéristiques.</Typography>
+      </ActionLayout>
+    </PlayBox>
+  );
 }
 
 const CustomCheckbox = styled(Checkbox)(() => ({
-    path: {
-        color: "#C4C4C4",
-    },
+  path: {
+    color: "#C4C4C4",
+  },
 }));
 
 function ActionLayout({
-    children,
-    title,
+  children,
+  title,
 }: {
-    children: JSX.Element;
-    title: string;
+  children: JSX.Element;
+  title: string;
 }) {
-    const [checked, setChecked] = React.useState(false);
+  const [checked, setChecked] = React.useState(false);
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setChecked(event.target.checked);
-    };
-    return (
-        <Box
-            sx={{
-                mb: 2,
-                borderRadius: "5px",
-                border: "3px solid #F9C74F",
-                padding: 1,
-            }}
-        >
-            <Typography alignItems="center" display="flex" variant="h6">
-                <InfoIcon sx={{ mr: 1 }} />
-                {title}
-                <CustomCheckbox
-                    checked={checked}
-                    onChange={handleChange}
-                    inputProps={{ "aria-label": "controlled" }}
-                />
-            </Typography>
-            <Box display="flex" alignItems="center" mt={1}>
-                Cout:
-                <Rating name="action-points-cost" readOnly max={3} value={3} />
-                <PaidIcon />
-                {`${2.19}€/j`}
-            </Box>
-        </Box>
-    );
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+  return (
+    <Box
+      sx={{
+        mb: 2,
+        borderRadius: "5px",
+        border: "3px solid #F9C74F",
+        padding: 1,
+      }}
+    >
+      <Typography alignItems="center" display="flex" variant="h6">
+        <InfoIcon sx={{ mr: 1 }} />
+        {title}
+        <CustomCheckbox
+          checked={checked}
+          onChange={handleChange}
+          inputProps={{ "aria-label": "controlled" }}
+        />
+      </Typography>
+      <Box display="flex" alignItems="center" mt={1}>
+        Cout:
+        <Rating name="action-points-cost" readOnly max={3} value={3} />
+        <PaidIcon />
+        {`${2.19}€/j`}
+      </Box>
+    </Box>
+  );
 }
