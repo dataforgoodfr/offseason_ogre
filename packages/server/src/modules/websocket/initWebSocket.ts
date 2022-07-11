f2cd77d84720ff7f065052175b7c47b51a31d732
Baboo7 <dbsharp404@gmail.com>
2022-07-11 18:32:05 +0200

    feat: Create endpoints to retrieve and update player actions

--- packages/server/src/modules/websocket/initWebSocket.ts
+++ packages/server/src/modules/websocket/initWebSocket.ts
@@ -3,6 +3,7 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import { handleJoinGame } from "./eventHandlers/joinGameHandler";
 import { handleUpdateGame } from "./eventHandlers/updateGameHandler";
+import { handleUpdatePlayerActions } from "./eventHandlers/updatePlayerActionsHandler";
 
 export { initWebSocket };
 
@@ -13,6 +14,7 @@ function initWebSocket({ app }: { app: Express }) {
   io.on("connection", (socket) => {
     handleJoinGame(socket);
     handleUpdateGame(socket);
+    handleUpdatePlayerActions(socket);
   });
   return { httpServer };
 }
