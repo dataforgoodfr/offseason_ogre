996bbb24551a5c19fbba48d326150b4de3d4d5c6
Vladimir Nafissi <vlad.nafissi@gmail.com>
2022-07-29 19:52:44 +0200

    adding dialog modal to validate actions

--- packages/client/src/modules/play/playerActions/Validation.tsx
+++ packages/client/src/modules/play/playerActions/Validation.tsx
@@ -1,20 +1,44 @@
-import { Box, Grid, Button, useTheme } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import { Dialog, DialogTitle, DialogActions } from "@mui/material";
+import { useState } from "react";
+
 export { ValidateActions };
 
 function ValidateActions() {
-  const theme = useTheme();
+  const [open, setOpen] = useState<boolean>(false);
+  const handleClickOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <Button
-      variant="contained"
-      color="actionValidation"
-      sx={{
-        mt: 2,
-        width: "200px",
-        height: "3rem",
-      }}
-    >
-      {" "}
-      Terminer le tour
-    </Button>
+    <Box>
+      <Button
+        variant="contained"
+        color="actionValidation"
+        sx={{
+          mt: 2,
+          width: "200px",
+          height: "3rem",
+        }}
+        onClick={handleClickOpen}
+      >
+        Terminer le tour
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          Valider les actions définitivement ?
+        </DialogTitle>
+        <DialogActions>
+          <Button onClick={handleClose}>Annuler</Button>
+          <Button onClick={handleClose} autoFocus>
+            Continuer
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
   );
 }
