d82bc4f8d414aa232b538e8a65199919154ff63e
louis-sanna-eki <louis.sanna@gmail.com>
2022-09-16 19:17:12 +0200

    Type socket data

--- packages/server/src/modules/websocket/utils.ts
+++ packages/server/src/modules/websocket/utils.ts
@@ -1,20 +1,16 @@
-/* eslint-disable no-param-reassign */
+import { z } from "zod";
 import { Socket, SocketData } from "./types";
 
 export { getSocketData };
 
-function getSocketData(socket: Socket) {
-  const keys: (keyof SocketData)[] = ["gameId", "user"];
-
-  return keys.reduce(
-    (data: SocketData, key: keyof SocketData) => {
-      if (socket.data[key] == null) {
-        throw new Error(`Socket data missing: ${key}`);
-      }
-      data[key] = socket.data[key] as any;
-      return data;
-    },
-
-    {} as SocketData
-  );
+function getSocketData(socket: Socket): SocketData {
+  const userSchema = z.object({
+    id: z.number(),
+    country: z.string(),
+    email: z.string(),
+    firstName: z.string(),
+    lastName: z.string(),
+    isTeacher: z.boolean(),
+  });
+  return z.object({ gameId: z.number(), user: userSchema }).parse(socket.data);
 }
