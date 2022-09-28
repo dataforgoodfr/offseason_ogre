836c1779920250bc745d6c2942f080c72b5ec98a
louis-sanna-eki <louis.sanna@gmail.com>
2022-09-28 21:04:07 +0200

    Remove old dev migrations

--- packages/server/prisma/migrations/20220627163306_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Game" ADD COLUMN     "isStepActive" BOOLEAN NOT NULL DEFAULT false;
--- packages/server/prisma/migrations/20220923160027_aelast_finished_step/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isStepActive` on the `Game` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Game" DROP COLUMN "isStepActive",
-ADD COLUMN     "lastFinishedStep" INTEGER NOT NULL DEFAULT 0;
