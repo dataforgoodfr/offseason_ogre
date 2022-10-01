c8351155b1b19e3830c471ce472e7252d3c49ff0
Baptiste Studer <dbsharp404@gmail.com>
2022-10-01 11:57:39 +0200

    fix(PlayerHeaderGrid): Prevent header from jumping

--- packages/client/src/modules/play/PlayerPersona/PlayerHeaderGrid.styles.tsx
+++ packages/client/src/modules/play/PlayerPersona/PlayerHeaderGrid.styles.tsx
@@ -4,11 +4,13 @@ import { styled } from "@mui/material/styles";
 export { CustomGrid };
 
 const CustomGrid = styled(Grid)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  paddingTop: theme.spacing(4),
   [theme.breakpoints.up("sm")]: {
     "--top": `${theme.variables.headerHeight.default}px`,
     position: "sticky",
     top: "var(--top)",
-    height: "calc(100vh - var(--top))",
-    paddingBottom: theme.spacing(2),
+    height: `calc(100vh - var(--top) - ${theme.spacing(4)})`,
   },
 }));
