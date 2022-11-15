9ef044b43a77e9dced76323b4615296df50b19ae
remiriv <remi.riviere.free@gmail.com>
2022-11-16 00:22:30 +0100

    Choice of personalization (#168)
    * Add button to prepare game
    
    * add tests to precommit
    
    * style: remove unused variables
    
    * style: remove unused variables
    
    * feat: Add form questions for each part
    
    * Revert "feat: Add form questions for each part"
    
    This reverts commit 1efa0c33757dd55d61b7fb6f078adde72e507547.
    
    * PR comments
    
    * Make button flex and add link to oilgre description
    
    Co-authored-by: Remi <remi.riviere@marcel.cab>

---
+++ packages/client/src/modules/play/Personalization/PersonalizationChoice.tsx
@@ -0,0 +1,112 @@
+import { Link } from "react-router-dom";
+import { Grid, Typography } from "@mui/material";
+import {
+  CustomContainer,
+  CentralContainer,
+  ChoiceText,
+  ChoiceButton,
+} from "./styles/personalization";
+import { Icon } from "../../common/components/Icon";
+import { useGameId } from "./hooks/useGameId";
+import { BackArrow } from "./common/BackArrow";
+
+export { PersonalizationChoice };
+
+function PersonalizationChoice() {
+  const gameId = useGameId();
+
+  return (
+    <CustomContainer maxWidth="lg">
+      <BackArrow />
+      <Typography variant="h5" color="secondary" sx={{ textAlign: "center" }}>
+        Préparer l'atelier
+      </Typography>
+      <CentralContainer>
+        <Grid container direction="row">
+          <Grid item xs={12} sm={2}>
+            <img
+              style={{ border: "2px solid white", borderRadius: "5px" }}
+              src="/sage.png"
+              alt="sage"
+            />
+          </Grid>
+          <Grid item xs={12} sm={10}>
+            <ChoiceText>
+              Pour une expérience personnalisée, je te propose de remplir un
+              formulaire afin de bâtir ton profil de consommation personnalisé.
+              Cela ne te prendra que quelques minutes. Si le formulaire n'est
+              pas complété et transmis à l'animateur, tu joueras avec le persona
+              Oil'gre.
+            </ChoiceText>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-around"
+          sx={{ mt: 4 }}
+        >
+          <Grid
+            item
+            sx={{
+              alignItems: "center",
+              display: "flex",
+              justifyContent: "center",
+              pt: 4,
+              pb: 2,
+              pr: 5,
+              pl: 5,
+            }}
+            xs={12}
+            sm={6}
+          >
+            <ChoiceButton
+              component={Link}
+              color="secondary"
+              variant="contained"
+              to={`/play/games/${gameId}/personalize/form`}
+            >
+              <Icon name="player-add" sx={{ mr: 1 }} />
+              <br />
+              <Typography>Créer mon profil conso</Typography>
+            </ChoiceButton>
+          </Grid>
+          <Grid
+            item
+            sx={{
+              alignItems: "center",
+              display: "flex",
+              justifyContent: "center",
+              pt: 4,
+              pb: 2,
+              pr: 5,
+              pl: 5,
+            }}
+            xs={12}
+            sm={6}
+          >
+            <ChoiceButton
+              color="secondary"
+              variant="contained"
+              component="a"
+              target="_blank"
+              href="https://drive.google.com/file/d/1pPVQt3EoKUmNwwB89Qde1OHAByEWSArd/view?usp=sharing"
+            >
+              <img
+                style={{
+                  height: "32px",
+                  marginLeft: "2%",
+                  marginRight: "2%",
+                  borderRadius: "5px",
+                }}
+                src="/avatar.png"
+                alt="avatar"
+              />
+              <Typography>Voir le persona Oil'gre</Typography>
+            </ChoiceButton>
+          </Grid>
+        </Grid>
+      </CentralContainer>
+    </CustomContainer>
+  );
+}
---
+++ packages/client/src/modules/play/Personalization/common/BackArrow.tsx
@@ -0,0 +1,24 @@
+import { Grid, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Icon } from "../../../common/components/Icon";
+
+export const BackArrow = () => {
+  return (
+    <Grid item display="flex" xs={2}>
+      <Button
+        component={Link}
+        color="secondary"
+        variant="contained"
+        to={`/play/games`}
+        sx={{
+          ml: "auto",
+          margin: "15px 0 35px 0",
+          padding: "10px 20px 10px 20px",
+        }}
+      >
+        <Icon name="arrow-back" sx={{ mr: 1 }} />
+        Retour
+      </Button>
+    </Grid>
+  );
+};
