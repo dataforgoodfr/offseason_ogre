bce829b4cd2f7ce9abea6a43e24b5a3768df9999
Baptiste Studer <dbsharp404@gmail.com>
2022-12-13 14:08:13 +0100

    feat(administration): Display game code

---
+++ packages/client/src/modules/common/components/CopyToClipboard/CopyToClipboard.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Icon } from "../Icon";
+import { copyToClipboard } from "./utils";
+import { useTranslation } from "../../../translations/useTranslation";
+import { SuccessAlert } from "../../../alert";
+import { Host } from "./CopyToClipboard.styles";
+
+export { CopyToClipboard };
+
+function CopyToClipboard({
+  value,
+  className,
+}: {
+  value: string;
+  className?: string;
+}) {
+  const { t } = useTranslation();
+  const [showCopied, setShowCopied] = useState(false);
+
+  const onCopy = async (e: any) => {
+    e.stopPropagation();
+    await copyToClipboard(value);
+    setShowCopied(true);
+  };
+
+  return (
+    <Host
+      className={`${className} flex items-center justify-between`}
+      onClick={onCopy}
+    >
+      {value}
+      <Icon
+        className="copy-to-clipboard__icon"
+        name="copy"
+        fontSize="small"
+        onClick={onCopy}
+      />
+      {showCopied && (
+        <SuccessAlert
+          message={t("message.success.copied")}
+          onClose={() => {
+            setShowCopied(false);
+          }}
+        />
+      )}
+    </Host>
+  );
+}
