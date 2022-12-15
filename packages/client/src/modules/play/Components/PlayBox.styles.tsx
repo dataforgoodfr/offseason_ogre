837a4f628e0582a45937d8938a363572c7aa9e94
Baptiste Studer <dbsharp404@gmail.com>
2022-12-15 21:55:25 +0100

    feat(PlayerActionsPage): Make consumption header sticky

---
+++ packages/client/src/modules/play/Components/PlayBox.styles.tsx
@@ -0,0 +1,39 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { getStyledProps } from "../../../utils/theme";
+
+export { HeaderMask, HeaderSection };
+
+const HeaderSection = styled(
+  Box,
+  getStyledProps("headerSticky")
+)<{ headerSticky: boolean }>(({ theme, headerSticky }) => {
+  if (headerSticky) {
+    return {
+      position: "sticky",
+      // TODO: parametrize spacing based on page padding.
+      top: `calc(${theme.variables.headerHeight.sm}px + ${theme.spacing(2)})`,
+      zIndex: 1,
+      [theme.breakpoints.up("sm")]: {
+        // TODO: parametrize spacing based on page padding.
+        top: `calc(${theme.variables.headerHeight.default}px + ${theme.spacing(
+          4
+        )})`,
+      },
+    };
+  }
+
+  return {};
+});
+
+/**
+ * Element that hides the content scrolling behind the header when the header is sticky.
+ */
+const HeaderMask = styled(Box)(({ theme }) => ({
+  position: "absolute",
+  bottom: `calc(100% - ${theme.spacing(2)})`,
+  left: 0,
+  width: "100%",
+  height: 64,
+  backgroundColor: theme.palette.backgrounds.page,
+}));
--- packages/client/src/modules/play/Components/PlayBox.tsx
+++ packages/client/src/modules/play/Components/PlayBox.tsx
@@ -1,9 +1,52 @@
 import { Box, BoxProps, useTheme } from "@mui/material";
+import { ReactNode } from "react";
+import { HeaderMask, HeaderSection } from "./PlayBox.styles";
 
 export { PlayBox };
 
-function PlayBox(boxProps: BoxProps = {}) {
+function PlayBox({
+  header = null,
+  headerSticky = false,
+  children,
+  ...boxProps
+}: { header?: ReactNode; headerSticky?: boolean } & BoxProps = {}) {
   const theme = useTheme();
+
+  if (header) {
+    return (
+      <>
+        <HeaderSection headerSticky={headerSticky}>
+          <HeaderMask />
+          <Box
+            position="relative"
+            bgcolor={theme.palette.primary.main}
+            border="2px solid"
+            borderBottom="none"
+            borderColor="white"
+            borderRadius="10px 10px 0 0"
+            color="white"
+            p={2}
+            {...boxProps}
+          >
+            {header}
+          </Box>
+        </HeaderSection>
+        <Box
+          bgcolor={theme.palette.primary.main}
+          border="2px solid"
+          borderTop="none"
+          borderColor="white"
+          borderRadius="0 0 10px 10px"
+          color="white"
+          p={2}
+          {...boxProps}
+        >
+          {children}
+        </Box>
+      </>
+    );
+  }
+
   return (
     <Box
       bgcolor={theme.palette.primary.main}
@@ -13,6 +56,8 @@ function PlayBox(boxProps: BoxProps = {}) {
       color="white"
       p={2}
       {...boxProps}
-    ></Box>
+    >
+      {children}
+    </Box>
   );
 }
