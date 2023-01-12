eea5932f032a1b8bda0aeb13b498b600ebd64546
remiriv <remi.riviere.free@gmail.com>
2023-01-12 15:18:31 +0100

    D4 g 231 update cards (#218)
    * feat: Add v2 for production cards
    
    * Update conso cards

--- packages/server/src/database/seeds/actionsSeed.ts
+++ packages/server/src/database/seeds/actionsSeed.ts
@@ -33,7 +33,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
       description: "Réduire ses déplacements en avion de 50%",
       name: availableActions.REDUCE_PLANE_HALF,
       helpCardLink:
-        "https://drive.google.com/file/d/1o8og6uawrINEJKj55vndLjxSnHBTO731/view?usp=sharing",
+        "https://drive.google.com/file/d/1xsTVShg7gVW0MbRoVwYKzaSDyjfwUjKl/view?usp=share_link",
       actionPointCost: 2,
       points: 200,
       financialCost: 0,
@@ -42,7 +42,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
       description: "Consommer local et de saison",
       name: availableActions.LOCAL_CONSUMPTION,
       helpCardLink:
-        "https://drive.google.com/file/d/1314uo7ir07rpqAsz7fCfyTCXmjip69Jn/view?usp=sharing",
+        "https://drive.google.com/file/d/1cWqdCf2gaRxlqiDJksn2r1eBW3YzLluV/view?usp=share_link",
       actionPointCost: 2,
       points: 200,
       financialCost: 0,
@@ -51,7 +51,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
       description: "Acheter deux fois moins de vêtements et chaussures",
       name: availableActions.REDUCE_CLOTHING_HALF,
       helpCardLink:
-        "https://drive.google.com/file/d/1c00purj7qEyHKZcGNdqOxDqXM_Koxhbq/view?usp=sharing",
+        "https://drive.google.com/file/d/1_HjnnT1PReXwBZbpSkPKLxmg3150b5zy/view?usp=share_link",
       actionPointCost: 2,
       points: 200,
       financialCost: 0,
@@ -61,7 +61,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
         "Acheter moins d'équipements numériques et uniquement d'occasion",
       name: availableActions.REDUCE_NUMERIC,
       helpCardLink:
-        "https://drive.google.com/file/d/1_CEiKPAlSqnRrwHCM_J19O-ucwg6hluV/view?usp=sharing",
+        "https://drive.google.com/file/d/193t5vMEnZSM63crP_f8lcLwb-aT4QVbL/view?usp=share_link",
       actionPointCost: 2,
       points: 200,
       financialCost: 0,
@@ -71,7 +71,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
         "Arrêter de consommer des produits laitiers (inutile si déjà végétalien)",
       name: availableActions.STOP_MILK,
       helpCardLink:
-        "https://drive.google.com/file/d/1Ivyl3ZtLYPJL6qlEgRqFPj3gbq5aL4m7/view?usp=sharing",
+        "https://drive.google.com/file/d/1dnDPdDinE4RHNihhsAH0xdH7P9_hQR4d/view?usp=share_link",
       actionPointCost: 1,
       points: 100,
       financialCost: 0,
@@ -80,7 +80,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
       description: "Arrêter de consommer des œufs (inutile si déjà végétalien)",
       name: availableActions.STOP_EGGS,
       helpCardLink:
-        "https://drive.google.com/file/d/1YZ31Sc048Gz4YPl9WZ3BTh81LFSR1nkP/view?usp=sharing",
+        "https://drive.google.com/file/d/17KGVHphAIsM5qgrYrDNqBPWGR3oCi9MS/view?usp=share_link",
       actionPointCost: 1,
       points: 100,
       financialCost: 0,
@@ -89,7 +89,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
       description: "Arrêter les boissons en cannette",
       name: availableActions.STOP_CANS,
       helpCardLink:
-        "https://drive.google.com/file/d/1FTlgA5K4Oj529DaC9WEMhcjea6vxIB4d/view?usp=sharing",
+        "https://drive.google.com/file/d/1a3osQzEObej0rXNgdLnLrq8jgeh1c_1h/view?usp=share_link",
       actionPointCost: 1,
       points: 0,
       financialCost: 0,
@@ -99,7 +99,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
         "Arrêter de consommer de la viande (inutile si déjà végétalien)",
       name: availableActions.STOP_MEAT,
       helpCardLink:
-        "https://drive.google.com/file/d/10vFPVtlJaQByCi1LJNLiZUoKQwAnpGfM/view?usp=sharing",
+        "https://drive.google.com/file/d/16h1uaf99s1NQHyMEfJM-ASm9d_a4Zy4V/view?usp=share_link",
       actionPointCost: 2,
       points: 100,
       financialCost: 0,
@@ -108,7 +108,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
       description: "Passer au zéro déchet alimentaire",
       name: availableActions.ZERO_WASTE,
       helpCardLink:
-        "https://drive.google.com/file/d/1Ll9VaqNQpYss9hbjzVwQHNPel-3w1HsL/view?usp=sharing",
+        "https://drive.google.com/file/d/1FqqrV9pVPr_X6sPceZ7ZnEFILlkOfN8f/view?usp=share_link",
       actionPointCost: 2,
       points: 200,
       financialCost: 0,
@@ -117,7 +117,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
       description: "Réduire ses déplacements en train de 50%",
       name: availableActions.REDUCE_TRAIN_HALF,
       helpCardLink:
-        "https://drive.google.com/file/d/1qIn1WGJ3vmx0f1X26Q6tRbmdrrUYqo4r/view?usp=sharing",
+        "https://drive.google.com/file/d/16BQ3GUx0LACvmnEvIBNiV61Ph9-1wIEs/view?usp=share_link",
       actionPointCost: 1,
       points: 0,
       financialCost: 0,
@@ -126,7 +126,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
       description: "Pratiquer l'éco-conduite",
       name: availableActions.ECO_DRIVING,
       helpCardLink:
-        "https://drive.google.com/file/d/1dU53aruZq6HOL1HobmJPokV51aeejw5G/view",
+        "https://drive.google.com/file/d/1FS1oD5Il-j2M8NA2U7kklWc-8yC7n5wW/view?usp=share_link",
       actionPointCost: 1,
       points: 200,
       financialCost: 0,
@@ -135,7 +135,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
       description: "Réduire ses déplacements en voiture de 20%",
       name: availableActions.REDUCE_CAR_20,
       helpCardLink:
-        "https://drive.google.com/file/d/1hLnapQjnsYWg8sKRufi1fi4izn_UFXEI/view",
+        "https://drive.google.com/file/d/1JfswSvFEMlekhQ3ZYYmO933HFjaKJr-w/view?usp=share_link",
       actionPointCost: 2,
       points: 300,
       financialCost: 0,
@@ -145,7 +145,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
         "Passer au véhicule électrique (laisser la valeur 0 si déjà équipé)",
       name: availableActions.ELECTRIC_CAR,
       helpCardLink:
-        "https://drive.google.com/file/d/1g4PS6TGYgDnsVlZwgCu38u6FUbTZle1l/view",
+        "https://drive.google.com/file/d/1x2ph5rsK-VUADEtvfuIkynQS4DC2rsO2/view?usp=share_link",
       actionPointCost: 3,
       points: 0,
 
@@ -155,7 +155,7 @@ function getActionsDataForConsumptionStep1(): Action[] {
       description: "Garder sa voiture plus de 15 ans",
       name: availableActions.KEEP_CAR_15,
       helpCardLink:
-        "https://drive.google.com/file/d/1RKoo_seg5pyQf0YDLIYLGtNOnu_eEHI0/view?usp=sharing",
+        "https://drive.google.com/file/d/1nvK4QMe_VZzIscdd4c5jG3uQ7IdDswJe/view?usp=share_link",
       actionPointCost: 3,
       points: 0,
       financialCost: 0,
@@ -174,7 +174,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Trier ses déchets",
       name: availableActions.CONSUMPTION_SORT_WASTE,
       helpCardLink:
-        "https://drive.google.com/file/d/1ZNHdRR52nVwGm8LJICkxqbZ2yZ9rilg5/view?usp=sharing",
+        "https://drive.google.com/file/d/1zlW9Um4AiTk6yKENGJi5PXZ6tdCG-WJ0/view?usp=share_link",
       actionPointCost: 1,
       points: 0,
       financialCost: 0,
@@ -183,7 +183,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Supprimer les déplacements en avion",
       name: availableActions.TRANSPORT_STOP_PLANE,
       helpCardLink:
-        "https://drive.google.com/file/d/11wD5YIsyjyrLfjnJOPwhHHMrmQnJs2VC/view?usp=sharing",
+        "https://drive.google.com/file/d/11427RQghOR7Jbo-hmmLA6mkty-0O7PId/view?usp=share_link",
       actionPointCost: 3,
       points: 200,
       financialCost: 0,
@@ -192,7 +192,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Isoler les murs",
       name: availableActions.HOUSE_INSULATE_WALLS,
       helpCardLink:
-        "https://drive.google.com/file/d/12ZMvTij4dQCkUmLVcZP6AcqcIRVdGVT1/view?usp=sharing",
+        "https://drive.google.com/file/d/11iWqGde4FVYsJwUKcYCZt5k3NaKi9X4r/view?usp=share_link",
       actionPointCost: 1,
       points: 300,
       financialCost: 1.78,
@@ -201,7 +201,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Isoler la toiture",
       name: availableActions.HOUSE_INSULATE_ROOF,
       helpCardLink:
-        "https://drive.google.com/file/d/1Vv7RkWoNiv-j0itAiWf38vEmlzFGKulv/view?usp=sharing",
+        "https://drive.google.com/file/d/1jnh9UhhADO-vopQwvWtWsobs9dY3niTG/view?usp=share_link",
       actionPointCost: 1,
       points: 300,
       financialCost: 0.89,
@@ -210,7 +210,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Changer les menuiseries (fenêtres)",
       name: availableActions.HOUSE_CHANGE_WOODWORK_WINDOWS,
       helpCardLink:
-        "https://drive.google.com/file/d/1mpuu02kOdBCglW9Vdn3_zyhKAaouyV6b/view?usp=sharing",
+        "https://drive.google.com/file/d/1Wh8t39PBC6IdUy_ByNAo6OLFtQ4_1FSj/view?usp=share_link",
       actionPointCost: 1,
       points: 300,
       financialCost: 0.69,
@@ -219,7 +219,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Mettre un système de ventilation performant",
       name: availableActions.HOUSE_INSTALL_EFFICIENT_VENTILATION_SYSTEM,
       helpCardLink:
-        "https://drive.google.com/file/d/1jtcNJZlbHSHEFM94ELdycJ05775PpyOU/view?usp=sharing",
+        "https://drive.google.com/file/d/1ORl_tkkT4D75YQ-K3h5ma5dgo1gGzrdU/view?usp=share_link",
       actionPointCost: 1,
       points: 300,
       financialCost: 0.57,
@@ -228,7 +228,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Installer un chauffage élec/bois performant",
       name: availableActions.HOUSE_INSTALL_EFFICIENT_ELECTRIC_WOOD_HEATING_SYSTEM,
       helpCardLink:
-        "https://drive.google.com/file/d/1xJoKdThri7juZW4yPIC-g54xrQTo4433/view?usp=sharing",
+        "https://drive.google.com/file/d/1lahrX7EQCiGlOPmylcPqI_7ytAp0DCJI/view?usp=share_link",
       actionPointCost: 1,
       points: 200,
       financialCost: 2.85,
@@ -237,7 +237,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Vivre dans un logement à 19°C maximum",
       name: availableActions.HOUSE_19_DEGREES_MAX,
       helpCardLink:
-        "https://drive.google.com/file/d/1VcnnafKco94LCDcNyyd9QNB7IaeqQ3OP/view?usp=sharing",
+        "https://drive.google.com/file/d/1u9BgdS22UXraNFxNAEINCrnkHq8OMZZx/view?usp=share_link",
       actionPointCost: 2,
       points: 200,
       financialCost: 0,
@@ -246,7 +246,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Vivre dans un logement 2 fois plus petit",
       name: availableActions.HOUSE_REDUCE_SIZE_HALF,
       helpCardLink:
-        "https://drive.google.com/file/d/17CnmJ-zIRsyV_J3TNoHra7NhC9OBczv-/view?usp=sharing",
+        "https://drive.google.com/file/d/1PpkLPFUjTKK7RAdari_88wM-u-xJ1F25/view?usp=share_link",
       actionPointCost: 3,
       points: 200,
       financialCost: 0,
@@ -255,7 +255,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Arrêter la climatisation",
       name: availableActions.HOUSE_STOP_AIR_CONDITIONING,
       helpCardLink:
-        "https://drive.google.com/file/d/1kf5EYmAD0rTNxkA-OjlyEd4_Z1kLhdZB/view?usp=sharing",
+        "https://drive.google.com/file/d/1bbZK-rxQJoI3-8CgGD59VmoRoEtlO2gt/view?usp=share_link",
       actionPointCost: 2,
       points: 0,
       financialCost: 0,
@@ -264,7 +264,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Uniquement des ampoules LED",
       name: availableActions.HOUSE_ONLY_LEDS,
       helpCardLink:
-        "https://drive.google.com/file/d/1NPLf-pbYnhUqM3cR57KcSYA9k-ciXnAC/view?usp=sharing",
+        "https://drive.google.com/file/d/1IU04I8c8ZfX6pqHmguleEPbYN2Gh4G3c/view?usp=share_link",
       actionPointCost: 1,
       points: 0,
       financialCost: 0,
@@ -273,7 +273,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Une douche par jour de moins de 5 minutes",
       name: availableActions.HOUSE_ONE_SHOWER_5_MINUTES_MAX,
       helpCardLink:
-        "https://drive.google.com/file/d/1uPLfQs7Wc5ZDnJmpN4qyIKSbrgxcQwUt/view?usp=sharing",
+        "https://drive.google.com/file/d/1hNDeP_-BrW6jcwre6heV3JyHqghxguvg/view?usp=share_link",
       actionPointCost: 1,
       points: 0,
       financialCost: 0,
@@ -282,7 +282,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Réduire de 50% ses usages Internet (dont vidéos)",
       name: availableActions.DIGITAL_REDUCE_INTERNET_HALF,
       helpCardLink:
-        "https://drive.google.com/file/d/1DuJneNccDyjg_QtA7Btui-tUwdmfFd01/view?usp=sharing",
+        "https://drive.google.com/file/d/1l8oXs2DycpBu-JPycUChRqpLyp1vnH7M/view?usp=share_link",
       actionPointCost: 2,
       points: 0,
       financialCost: 0,
@@ -291,7 +291,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Uniquement de l'électroménager performant (A+++)",
       name: availableActions.HOUSE_EFFICIENT_HOUSEHOLD_APPLIANCES,
       helpCardLink:
-        "https://drive.google.com/file/d/1I582L5SFo1p4Hi5fT954E5QdloHy4eVK/view?usp=sharing",
+        "https://drive.google.com/file/d/1IriXin5FAGjpSfqMySzCrVbMzZjP8dxA/view?usp=share_link",
       actionPointCost: 2,
       points: 100,
       financialCost: 0,
@@ -300,7 +300,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Débrancher les appareils en veille",
       name: availableActions.HOUSE_UNPLUNG_APPLIANCES_ON_STANDBY,
       helpCardLink:
-        "https://drive.google.com/file/d/1wxUkBc9s7Bvjwu-6l0RfM6y7jLoqfGmZ/view?usp=sharing",
+        "https://drive.google.com/file/d/1jPS4oWZD3_nJHh_BoTo8tVle8HClvcOL/view?usp=share_link",
       actionPointCost: 1,
       points: 0,
       financialCost: 0,
@@ -309,7 +309,7 @@ function getActionsDataForConsumptionStep2(): Action[] {
       description: "Débrancher les chargeurs",
       name: availableActions.HOUSE_UNPLUNG_CHARGERS,
       helpCardLink:
-        "https://drive.google.com/file/d/1xElUTrG69kqOfQJmZ7lFVhb-7UNiKdzq/view?usp=sharing",
+        "https://drive.google.com/file/d/1tAa8_owb2uF3Ly_UlTtCq5cx4CUY13N-/view?usp=share_link",
       actionPointCost: 1,
       points: 0,
       financialCost: 0,
