bd88b3995fa4faf21ef6d121602efce249d9cc55
louis-sanna-eki <louis.sanna@gmail.com>
2023-02-05 18:37:14 +0100

    fix(limiter): fix broken import

--- packages/server/src/middlewares/limit.ts
+++ packages/server/src/middlewares/limit.ts
@@ -1,11 +1,11 @@
-import RateLimit from "express-rate-limit";
+import rateLimit from "express-rate-limit";
 
 const MAX_REQUEST_PER_MINUTE = 120;
 const WINDOW_IN_MS = 1 * 60 * 1000;
 
 export { limiter };
 
-const limiter = new (RateLimit as any)({
+const limiter = rateLimit({
   windowMs: WINDOW_IN_MS,
   max: MAX_REQUEST_PER_MINUTE,
 });
