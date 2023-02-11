429e458227ffee6c7de5ca9205e06647aafafb5a
louis-sanna-eki <louis.sanna@gmail.com>
2023-02-11 14:59:28 +0100

    feat(env): load env

--- packages/server/src/modules/tokens/index.ts
+++ packages/server/src/modules/tokens/index.ts
@@ -1,6 +1,9 @@
 import * as jwt from "jsonwebtoken";
 import invariant from "tiny-invariant";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config({ path: `${__dirname}/../../../.env` });
+
 invariant(
   process.env.SECRET_KEY !== undefined,
   "SECRET_KEY is missing in your environment"
