14eadaf83c1c415629682e6e9f5a9893ac81a73f
Baptiste Studer <dbsharp404@gmail.com>
2023-05-28 01:42:46 +0200

    fix(logoutController): Clear cookie

--- packages/server/src/modules/users/controllers/logoutController.ts
+++ packages/server/src/modules/users/controllers/logoutController.ts
@@ -3,6 +3,9 @@ import { Request, Response } from "express";
 export { logoutController };
 
 function logoutController(request: Request, response: Response) {
-  response.clearCookie("authentificationToken");
+  response.clearCookie("authentificationToken", {
+    sameSite: "none",
+    secure: true,
+  });
   response.status(200).send();
 }
