c8ace6ea8c7d3ed266790848fd254451e3a84c42
Baptiste Studer <dbsharp404@gmail.com>
2023-05-28 01:42:28 +0200

    fix(server): Update cors

--- packages/server/src/middlewares/cors.ts
+++ packages/server/src/middlewares/cors.ts
@@ -9,12 +9,12 @@ const cors = corsLib({
 
 function getOrigins() {
   if (process.env.NODE_ENV === "production") {
-    return ["https://app.atelierogre.org"];
+    return [/^https:\/\/app\.atelierogre\.org/];
   }
   if (process.env.NODE_ENV === "staging") {
     return [
-      "https://app-staging.atelierogre.org",
-      /^https:\/\/deploy-preview-\d+--atelierogre-staging\.netlify\.app$/,
+      /^https:\/\/app-staging\.atelierogre\.org/,
+      /^https:\/\/deploy-preview-\d+--atelierogre-staging\.netlify\.app/,
     ];
   }
   return "*";
