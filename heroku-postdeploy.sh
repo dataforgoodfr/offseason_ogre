ef609dfcfa859aa7fb04687e538bc4ab45902bbf
Baptiste Studer <dbsharp404@gmail.com>
2023-06-04 23:55:44 +0200

    chore(heroku): Set post deploy script permissions

--- heroku-postdeploy.sh
+++ heroku-postdeploy.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-echo "HEROKU_APP_NAME = $HEROKU_APP_NAME"
-echo "HEROKU_PR_NUMBER = $HEROKU_PR_NUMBER"
-
 # Set env var
 curl --location --request PATCH "https://api.heroku.com/apps/$HEROKU_APP_NAME/config-vars" \
 --header "Authorization: Bearer $HEROKU_API_TOKEN" \
