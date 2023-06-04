5f285397c3b8a76557aca19e77f8ee6adb55de47
Baptiste Studer <dbsharp404@gmail.com>
2023-06-04 22:52:01 +0200

    chore(netlify): Override build env var

--- packages/client/netlify/plugins/netlify-plugin-deploy-preview-env/index.js
+++ packages/client/netlify/plugins/netlify-plugin-deploy-preview-env/index.js
@@ -1,28 +1,41 @@
-const ENV_VAR_NAME = "VITE_API_BASE_URL";
-
 module.exports = {
-  onPreBuild: async ({ utils }) => {
+  onPreBuild: async ({ netlifyConfig, utils }) => {
+    const envVarOverrides = {
+      VITE_API_BASE_URL: `https://atelierogre-pr-${process.env.REVIEW_ID}.herokuapp.com`,
+    };
+
+    console.log(
+      `Setting env vars for branch ${process.env.HEAD} (PR ${process.env.REVIEW_ID})`
+    );
+
     try {
-      console.log(
-        `Setting env var ${ENV_VAR_NAME} for branch ${process.env.HEAD} (PR ${process.env.REVIEW_ID})`
-      );
-      await fetch(
-        `https://api.netlify.com/api/v1/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env/${ENV_VAR_NAME}?site_id=${process.env.SITE_ID}`,
-        {
-          method: "PATCH",
-          headers: {
-            Authorization: `Bearer ${process.env.NETLIFY_API_TOKEN}`,
-            "Content-type": "application/json; charset=UTF-8",
-          },
-          body: JSON.stringify({
-            context: "branch",
-            context_parameter: process.env.HEAD,
-            value: `https://atelierogre-pr-${process.env.REVIEW_ID}.herokuapp.com`,
-          }),
-        }
-      ).then((res) => res.json());
+      for (const [key, value] of Object.entries(envVarOverrides)) {
+        await overrideEnvVar(key, value, { netlifyConfig });
+      }
     } catch (err) {
       utils.build.failPlugin(err.message, { error: err });
     }
   },
 };
+
+async function overrideEnvVar(key, value, { netlifyConfig }) {
+  console.log(`Setting env var ${key}`);
+
+  netlifyConfig.build.environment[key] = value;
+
+  await fetch(
+    `https://api.netlify.com/api/v1/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env/${key}?site_id=${process.env.SITE_ID}`,
+    {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${process.env.NETLIFY_API_TOKEN}`,
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        context: "branch",
+        context_parameter: process.env.HEAD,
+        value,
+      }),
+    }
+  );
+}
