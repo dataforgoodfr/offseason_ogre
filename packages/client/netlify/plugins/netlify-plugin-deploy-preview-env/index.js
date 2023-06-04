486ad8d6f7bec83ece6db4fa80a9e27546f04a44
Baptiste Studer <dbsharp404@gmail.com>
2023-06-04 22:43:19 +0200

    chore(netlify): Export plugin with common js

--- packages/client/netlify/plugins/netlify-plugin-deploy-preview-env/index.js
+++ packages/client/netlify/plugins/netlify-plugin-deploy-preview-env/index.js
@@ -1,28 +1,28 @@
-export { onPreBuild };
-
 const ENV_VAR_NAME = "VITE_API_BASE_URL";
 
-async function onPreBuild({ utils }) {
-  try {
-    console.log(
-      `Setting env var ${ENV_VAR_NAME} for branch ${process.env.HEAD} (PR ${process.env.REVIEW_ID})`
-    );
-    await fetch(
-      `https://api.netlify.com/api/v1/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env/${ENV_VAR_NAME}?site_id=${process.env.SITE_ID}`,
-      {
-        method: "PATCH",
-        headers: {
-          Authorization: `Bearer ${process.env.NETLIFY_API_TOKEN}`,
-          "Content-type": "application/json; charset=UTF-8",
-        },
-        body: JSON.stringify({
-          context: "branch",
-          context_parameter: process.env.HEAD,
-          value: `https://atelierogre-pr-${process.env.REVIEW_ID}.herokuapp.com`,
-        }),
-      }
-    ).then((res) => res.json());
-  } catch (err) {
-    utils.build.failPlugin(err.message, { error: err });
-  }
-}
+module.exports = {
+  onPreBuild: async ({ utils }) => {
+    try {
+      console.log(
+        `Setting env var ${ENV_VAR_NAME} for branch ${process.env.HEAD} (PR ${process.env.REVIEW_ID})`
+      );
+      await fetch(
+        `https://api.netlify.com/api/v1/accounts/${process.env.NETLIFY_ACCOUNT_ID}/env/${ENV_VAR_NAME}?site_id=${process.env.SITE_ID}`,
+        {
+          method: "PATCH",
+          headers: {
+            Authorization: `Bearer ${process.env.NETLIFY_API_TOKEN}`,
+            "Content-type": "application/json; charset=UTF-8",
+          },
+          body: JSON.stringify({
+            context: "branch",
+            context_parameter: process.env.HEAD,
+            value: `https://atelierogre-pr-${process.env.REVIEW_ID}.herokuapp.com`,
+          }),
+        }
+      ).then((res) => res.json());
+    } catch (err) {
+      utils.build.failPlugin(err.message, { error: err });
+    }
+  },
+};
